684a40ff481b4d4abcf6c9133c8bdd5313e7bbd6
Jaziell Silva <JaziellSilva@hotmail.com>
2024-03-26 01:01:42 -0600

    Inicializacion Directa, Lectura Lista

--- src/main/java/com/huella/lector/Captura.java
+++ src/main/java/com/huella/lector/Captura.java
@@ -1,57 +1,141 @@
 package com.huella.lector;
 
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JPanel;
 
 import com.digitalpersona.uareu.*;
 
 //import java.awt.event.ActionListener;
 
-public class Captura extends JPanel implements ActionListener{
+public class Captura {
 	
-	private static final long serialVersionUID =2;
+	private static Reader reader = null;
+	private static Engine engine = null;
 	
-	private JDialog       m_dlgParent;
-	private CaptureThread m_capture;
-	private Reader        m_reader;
-	private ImagePanel    m_image;
-	private boolean       m_bStreaming;
+	/**
+	 * 
+	 */
+	public static void initializeReader() {
+        try {
+            ReaderCollection readers = UareUGlobal.GetReaderCollection();
+            readers.GetReaders();
+            if (readers.size() == 1) {
+                reader = readers.get(0); // Busca el lector y usa el unico conectado
+                reader.Open(Reader.Priority.EXCLUSIVE); //Abre y toma el unico lector dispoible
+                Reader.Status status = reader.GetStatus();
+                System.out.println("Estado del Lector: " + status.status);//Regresa Status de Lector
+            } else {
+                System.out.println("No se encontró el lector o hay múltiples lectores conectados VERIFIQUE CONEXION.");//Esta por demass
+                }
+        } catch (UareUException e) {
+            e.printStackTrace();
+            // excepciones específicas del SDK
+        }
+    }
 	
-	private Capture(Reader reader, boolean bStreaming){
-		m_reader = reader;
-		m_bStreaming = bStreaming;
-		
-		m_capture = new CaptureThread(m_reader, m_bStreaming, Fid.Format.ANSI_381_2004, Reader.ImageProcessing.IMG_PROC_DEFAULT);
+	public static Fid captureFingerprint() {
+	    Fid fid = null;
+	    try {
+	        if (reader != null) {
+	            System.out.println("Por favor, coloca firmemente tu dedo en el lector");
+	            // Inicia un bucle que intentará capturar la huella dactilar
+	            while (true) {
+	                Reader.CaptureResult cr = reader.Capture(
+	                        Fid.Format.ISO_19794_4_2005, //FORMATO ISO CON ALCANCE GLOBAL	                        
+	                        Reader.ImageProcessing.IMG_PROC_DEFAULT, // Procesamiento de imagen por defecto
+	                        500, // Resolución en DPI(MAXIMA)
+	                        -1   // Timeout, -1 para indefinido
+	                );
 
-		final int vgap = 5;
-		BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
-		setLayout(layout);
+//	                // Verifica la calidad de la captura
+//	                if (cr.quality == Reader.CaptureQuality.GOOD) {
+//	                    System.out.println("Captura de huella exitosa.");
+//	                    fid = cr.image; // Guarda la imagen de la huella
+//	                    break; // Sal del bucle si la captura es exitosa
+//	                } else {
+//	                    System.out.println("Captura no exitosa. Intenta nuevamente...");
+//	                    // Puedes decidir si quieres que el usuario intente nuevamente automáticamente
+//	                    // o si prefieres que realice alguna acción para reintentar.
+//	                }
+//	            }
+//	        }
+//	    } catch (UareUException e) {
+//	        System.err.println("Error al capturar la huella dactilar: " + e.getMessage());
+//	        e.printStackTrace();
+//	    }
+//	    return fid; // Devuelve la imagen de la huella o null si no se pudo capturar
+//	}	
 
-		m_image = new ImagePanel();
-		Dimension dm = new Dimension(400, 500);
-		m_image.setPreferredSize(dm);
-		add(m_image);
-		add(Box.createVerticalStrut(vgap));
-		
-		JButton btnBack = new JButton("Back");
-		btnBack.setActionCommand(ACT_BACK);
-		btnBack.addActionListener(this);
-		add(btnBack);
-		add(Box.createVerticalStrut(vgap));
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	 
+	                switch (cr.quality) {
+                    case GOOD:
+                        System.out.println("Captura de huella exitosa.");
+                        fid = cr.image;
+                        return fid;
+                    case CANCELED:
+                        System.out.println("Captura cancelada.");
+                        return null;
+                    case FAKE_FINGER:
+                        System.out.println("Se detectó un dedo falso (O MUERTO).");
+                        break;
+                    case NO_FINGER:
+                        System.out.println("No se detectó el dedo. EL MOCHO");
+                        break;
+                    case READER_DIRTY:
+                        System.out.println("El lector necesita limpieza. PUERCO XDD");
+                        break;
+                    case FINGER_TOO_LEFT:
+                    case FINGER_TOO_RIGHT:
+                    case FINGER_TOO_HIGH:
+                    case FINGER_TOO_LOW:
+                        System.out.println("El dedo no está colocado correctamente en el lector. PONGALO BIEN");
+                        break;
+                    case TIMED_OUT:
+                        System.out.println("La operación ha excedido el tiempo de espera.");
+                        return null;
+                    // Casos segun la ocasion
+                    default:
+                        System.out.println("Error desconocido. Intenta de nuevo.");
+                        break;
+                }
+                // Da al usuario la oportunidad de ajustar su dedo o solucionar el problema antes de reintentar
+                Thread.sleep(1000); // Espera un segundo antes de reintentar
+            }
+        }
+    } catch (UareUException e) {
+        System.err.println("Error al capturar la huella dactilar: " + e.getMessage());
+        e.printStackTrace();
+    } catch (InterruptedException e) {
+        System.err.println("La captura fue interrumpida.");
+        Thread.currentThread().interrupt();
+    }
+    return null;
+}
+    public static Fmd createFmdFromFid(Fid fid) {
+        Fmd fmd = null;
+        try {
+            fmd = engine.CreateFmd(fid, Fmd.Format.ISO_19794_2_2005);
+        } catch (UareUException e) {
+            System.err.println("Error al crear FMD desde FID: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return fmd;
+    }
+	                
+	                
+    public static void closeReader() {	//Sera util mas adelante
+        if (reader != null) {
+            try {
+                reader.Close();
+                System.out.println("Lector cerrado correctamente.");
+            } catch (UareUException e) {
+                e.printStackTrace();
+                // excepciones específicas del SDK al cerrar el lector
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        initializeReader();
+        Fid fingerprintImage = captureFingerprint(); // Asegúrate de añadir esta línea
+        // Procesa la imagen capturada como sea necesario
+        closeReader(); // Limpieza al final
+    }
 }
